34798b4f917189883022269f71c6e7b63e6477f3
lucovilha <luejirafael1@gmail.com>
2026-06-03 20:09:07 +0100

    adicionar página de reservas do cliente e limpar ficheiros extra

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?= date('Y') ?> Hotel Bacalhau. Todos os direitos reservados.</p>
-        </div>
-    </footer>
-</body>
-</html>
