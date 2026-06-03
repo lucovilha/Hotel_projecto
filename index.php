c01014ce04d29085284f46e4368468c804e29d03
lucovilha <luejirafael1@gmail.com>
2026-06-03 23:30:05 +0100

    aplicar CSS na homepage

--- index.php
+++ index.php
@@ -7,23 +7,30 @@
  <head> 
      <meta charset="UTF-8"> 
      <title>Hotel</title> 
+     <link rel="stylesheet" href="includes/style.css"> 
  </head> 
  <body> 
-     <h1>Bem-vindo ao Hotel</h1> 
-    <a href="sobre.php">Sobre Nós</a> 
-    <hr> 
-     <?php if (esta_logado()): ?> 
-         <p>Olá, <?= h(user_nome()) ?>!</p> 
-         <?php if (e_staff()): ?> 
-             <a href="admin/index.php">Ir para o Backoffice</a> 
+     <header> 
+         <a href="index.php">Hotel</a> 
+         <a href="sobre.php">Sobre Nós</a> 
+         <?php if (esta_logado()): ?> 
+             <?php if (e_staff()): ?> 
+                 <a href="admin/index.php">Backoffice</a> 
+             <?php else: ?> 
+                 <a href="cliente/reservas.php">As Minhas Reservas</a> 
+             <?php endif; ?> 
+             <a href="auth/logout.php">Sair</a> 
          <?php else: ?> 
-             <a href="cliente/reservas.php">As Minhas Reservas</a> 
+             <a href="auth/login.php">Entrar</a> 
+             <a href="auth/register.php">Registar</a> 
          <?php endif; ?> 
-         <br> 
-         <a href="auth/logout.php">Sair</a> 
-     <?php else: ?> 
-         <a href="auth/login.php">Entrar</a> | 
-         <a href="auth/register.php">Registar</a> 
-     <?php endif; ?> 
+     </header> 
+     <main> 
+         <h1>Bem-vindo ao Hotel</h1> 
+         <p>O melhor hotel para a tua estadia.</p> 
+     </main> 
+     <footer> 
+         <p>&copy; 2026 Hotel. Todos os direitos reservados.</p> 
+     </footer> 
  </body> 
- </html> 
+ </html>
