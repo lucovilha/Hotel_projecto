6b5f64d4d428cbd6964ef752e44a095b9e7cd9ed
lucovilha <luejirafael1@gmail.com>
2026-06-03 19:49:03 +0100

    adicionar dashboard admin e ficheiros de layout

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+    </main>
+    <footer>
+        <div class="container">
+            <p>&copy; <?= date('Y') ?> Hotel Bacalhau. Todos os direitos reservados.</p>
+        </div>
+    </footer>
+</body>
+</html>
